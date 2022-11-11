683c4fe5986d80a5a14a7f2deb477a50d1fd8d76
Paulo Cesar Goncalves Nicolau <paugonca@c1r16s9.42lisboa.com>
2022-11-11 17:22:26 +0000

    Advanced some development in get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,28 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 16:52:21 by paugonca          #+#    #+#             */
-/*   Updated: 2022/11/07 15:49:52 by paugonca         ###   ########.fr       */
+/*   Updated: 2022/11/11 17:09:50 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*store_log(char	**log, char **buf, size_t p)
+{
+	char	*res;
+	char	*hold;
+
+	res = NULL;
+	if (p <= 0)
+	{
+		if (p == 0 && *log)
+		{
+			res = (*log);
+			*log = NULL;
+		}
+		return (res);
+	}
+}
 
 char	*get_next_line(int fd)
 {
@@ -18,4 +35,16 @@ char	*get_next_line(int fd)
 	char		*buf;
 	char		*res;
 	size_t		p;
+
+	if (read(fd, 0, 0) == -1 || (fd < 0 || fd > 1024) || BUFFER_SIZE <= 0)
+		return (0);
+	p = 1;
+	res = NULL;
+	buf = ft_strchr(log, '\n');
+	if (!buf)
+	{
+		buf = malloc(BUFFER_SIZE + 1);
+		if (!buf)
+			return (0);
+	}
 }
