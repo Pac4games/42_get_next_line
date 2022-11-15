14e15f3bd66c8dc55fd5ab6ad1a9cee7eb94e729
Paulo Cesar Goncalves Nicolau <paugonca@c1r19s6.42lisboa.com>
2022-11-15 12:06:28 +0000

    Added some more stuff to get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,31 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 16:52:21 by paugonca          #+#    #+#             */
-/*   Updated: 2022/11/11 17:09:50 by paugonca         ###   ########.fr       */
+/*   Updated: 2022/11/14 11:49:59 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*check_buf(char **log, char **buf, size_t len)
+{
+	char	*res;
+	char	*hold;
+	size_t	p;
+
+	p = ft_strlen(*log);
+	res = malloc(len + p + 1);
+	if (!res)
+		return (0);
+	ft_memcpy(res, *log, p);
+	ft_memcpy(res + p, *buf, len);
+	res[len + p] = '\0';
+	hold = ft_strdup((*buf) + len);
+	if (*log)
+		free (*log);
+	(*log) = hold;
+	return (res);
+}
 
 static char	*store_log(char	**log, char **buf, size_t p)
 {
@@ -27,6 +47,18 @@ static char	*store_log(char	**log, char **buf, size_t p)
 		}
 		return (res);
 	}
+	(*buf)[p] = '\0';
+	hold = ft_strchr(*bufer, '\n');
+	if (hold)
+		res = check_buf(log, buf, (hold - *buf) + 1);
+	else
+	{
+		hold = ft_strjoin(*log, *buf);
+		if (*log)
+			free(*log);
+		*log = hold;
+	}
+	return (res);
 }
 
 char	*get_next_line(int fd)
@@ -46,5 +78,12 @@ char	*get_next_line(int fd)
 		buf = malloc(BUFFER_SIZE + 1);
 		if (!buf)
 			return (0);
+		while (res == NULL && p > 0)
+		{
+			p = read(fd, buf, BUFFER_SIZE);
+			res = store_log(&log, &buf, p);
+		}
+		free(buf);
+		return (res);
 	}
 }
