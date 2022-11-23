9a490d5f24426290c348fc9204245bd892e9b06d
Paulo Cesar Goncalves Nicolau <paugonca@c1r17s11.42lisboa.com>
2022-11-23 13:03:37 +0000

    Tested the mandatory part and removed unecessary BUFFER_SIZE declaration in get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,18 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:07:52 by paugonca          #+#    #+#             */
-/*   Updated: 2022/11/16 16:53:36 by paugonca         ###   ########.fr       */
+/*   Updated: 2022/11/23 13:01:19 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
-# endif
-
 # include <unistd.h>
 # include <stdlib.h>
 
+//get_next_line
+char	*get_next_line(int fd);
 //get_next_line_utils.c
 size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char *s1, char *s2);
